d108f0c3b16f1fd48b8c2010eea541f4370d6b21
bladeofcadence <bladeofcadence@gmail.com>
2022-11-18 15:04:10 -0600

    kn initial

--- xcodeDice/ViewController.swift
+++ xcodeDice/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var DICe: UIImageView!
+    @IBOutlet var Lbel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonPress()
+    {
+        RollDice()
+    }
+    
+    func RollDice()
+    {
+        let Dnom = Int.random(in: 1...20)
+        let imageName = "d\(Dnom)"
+        
+        switch Dnom
+        {
+        case 1:
+            Lbel.isHidden = false
+            Lbel.text = "You suck"
+            break
+        case 20:
+            Lbel.isHidden = false
+            Lbel.text = "You're the cheese"
+            break
+        default:
+            Lbel.isHidden = true
+            break
+        }
+        
+        DICe.image = UIImage(named: imageName)
+    }
+    
 }
 
