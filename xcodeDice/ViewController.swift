d2a658657217806e063ff9c0e8d0392f4740f461
bladeofcadence <bladeofcadence@gmail.com>
2022-11-21 21:34:59 -0600

    what if, what if i was a unicorn

--- xcodeDice/ViewController.swift
+++ xcodeDice/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var DICe: UIImageView!
     @IBOutlet var Lbel: UILabel!
-    
-    
+    var audioPlayer: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,8 +23,22 @@ class ViewController: UIViewController {
         RollDice()
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?)
+    {
+        if (motion == .motionShake)
+        {
+            RollDice();
+        }
+    }
+    
     func RollDice()
     {
+        
+        var url: URL
+        let pathToSound = Bundle.main.path(forResource: "rolldice", ofType: "mp3")!
+        let failpath = Bundle.main.path(forResource: "failwah", ofType: "mp3")!
+        let winpath = Bundle.main.path(forResource: "zfanfare", ofType: "mp3")!
+        
         let Dnom = Int.random(in: 1...20)
         let imageName = "d\(Dnom)"
         
@@ -34,14 +47,27 @@ class ViewController: UIViewController {
         case 1:
             Lbel.isHidden = false
             Lbel.text = "You suck"
+            url = URL(fileURLWithPath: failpath)
             break
         case 20:
             Lbel.isHidden = false
             Lbel.text = "You're the cheese"
+            url = URL(fileURLWithPath: winpath)
             break
         default:
             Lbel.isHidden = true
+            url = URL(fileURLWithPath: pathToSound)
             break
+        }
+        
+        do
+        {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        }
+        catch
+        {
+            print("error babe")
         }
         
         DICe.image = UIImage(named: imageName)
